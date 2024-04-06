41915bebb547bb31264f228b8f49fb8bc09b0971
Yusuf Gunawan <yusufmg27@gmail.com>
2024-04-06 13:25:59 +0700

    migration

---
+++ database/migrations/2024_04_05_130549_create_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('order_code')->unique();
+            $table->integer('quantity');
+            $table->string('costumer_id')->index();
+            $table->string('service_id')->index();
+            $table->string('status');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
